48fa7a792d73ff35e2f00ab8e729df6990a89b83
David Auza <davidauza01@gmail.com>
2021-11-01 18:27:40 -0500

    Implement app requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+
+interface IAppForm {
+  firstInputValue: string;
+  secondInputValue: string;
+}
 
 function App() {
+  const [appForm, setAppForm] = useState<IAppForm>({
+    firstInputValue: '',
+    secondInputValue: ''
+  });
+
+  const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setAppForm(
+        {
+          ...appForm,
+          [event.target.id]: event.target.value
+        }
+    )
+  };
+
+  const handleFormSubmit = (event: any) => {
+    event.preventDefault();
+    setAppForm(
+        {
+          firstInputValue: appForm.secondInputValue,
+          secondInputValue: appForm.firstInputValue
+        }
+    )
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App container d-flex justify-content-center align-items-center">
+        <Form onSubmit={handleFormSubmit} className="w-50">
+          <FormGroup>
+            <Label for="firstInputValue">
+              First Text Area
+            </Label>
+            <Input
+                id="firstInputValue"
+                name="firstInputValue"
+                type="textarea"
+                value={appForm.firstInputValue}
+                onChange={handleFormChange}
+            />
+          </FormGroup>
+          <Button className="my-5">
+            Swap
+          </Button>
+          <FormGroup>
+            <Label for="secondInputValue">
+              Second Text Area
+            </Label>
+            <Input
+                id="secondInputValue"
+                name="secondInputValue"
+                type="textarea"
+                value={appForm.secondInputValue}
+                onChange={handleFormChange}
+            />
+          </FormGroup>
+        </Form>
+      </div>
   );
 }
 
